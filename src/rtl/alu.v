651cf54b8e9a8a5efc768f2b93eec1cdcc7e6ea4
dpapp-ux <dpapp@bu.edu>
2025-12-01 16:05:18 -0500

    Add files via upload

--- src/rtl/alu.v
+++ src/rtl/alu.v
@@ -7,8 +7,25 @@ module alu(
     input wire signed [`XLEN-1:0] i_data_1, // First input data to operate upon (usually rs1)
     input wire signed [`XLEN-1:0] i_data_2, // Second input data to operate upon (usually rs2 or immediate)
 
+    input wire[31:0] i_rs1, //Rs1 From Decode
+    input wire[31:0] i_rs2, //Rs2 From Decode
+    
+    input wire[4:0] i_rs1_addr, //address for register source 1
+    
+    input wire[31:0] i_imm_de, //Immediate value from decode
+    input wire[2:0] i_funct3_de, //function type from decode
+    input wire[`Oplen:0] i_opcode_de, //opcode type from decode
+    input wire[`Oplen:0] i_opcode_de, //opcode type from decode
+
     /* ALU output registers */
+    output reg[31:0] or_rs1, //Source register 1 value
+    output reg[31:0] or_rs2, //Source register 2 value
+    output reg[11:0] or_imm, //Immediate value
+    output reg[2:0] or_funct3, // function type
+    output reg[4:0] or_rs1_addr, //address for register source 1
+    output reg[`Oplen:0] or_opcode, //opcode type 
     output reg [`XLEN-1:0] ow_result // ALU result
+    
 );
 
 wire [`XLEN-1:0] i_data_1_us = $unsigned(i_data_1);
