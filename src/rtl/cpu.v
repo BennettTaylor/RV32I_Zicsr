739e111f6c857158b76d31068b3b1792e5aaac31
Pappademos <dpapp@vlsi57.bu.edu>
2025-12-05 11:02:16 -0500

    added changes to cpu

--- src/rtl/cpu.v
+++ src/rtl/cpu.v
@@ -9,30 +9,46 @@ module cpu(
     /*This is from angeloJacobs, the rest is from ours */
   
 );
-
-//Wires for fetch
-wire[31:0] fetch_pc;
-wire[31:0] fetch_inst;
+// Wires for Program Memory
+wire pm_awk // Program Mem Awk
+wire [31:0] pm_instruction // Output Instruction
 
 //Wires for decode
-wire[`Oplen-1:0] decoder_opcode;
-wire[31:0] decoder_pc;
-wire[4:0] decoder_rs1_addr, decoder_rs2_addr;
-wire[4:0] decoder_rs1_addr_q, decoder_rs2_addr_q;
-wire[4:0] decoder_rd_addr; 
-wire[31:0] decoder_imm; 
-wire[2:0] decoder_funct3;
-wire decoder_ce;
-wire decoder_flush;
+wire decode_or_opcode; // Opcode
+wire decode_or_rd_addr; // Destination register address
+wire decode_or_rs1_addr; // Source register 1 address for forwarding
+wire decode_or_rs2_addr; // Source register 2 address for forwarding
+wire decode_or_rs1_data; // Source register 1 value
+wire decode_or_rs2_data; // Source register 2 value
+wire decode_or_imm; // Immediate value
+wire decode_or_funct7; // Funct7
+wire decode_or_funct3; // Funct3
+wire decode_or_alu_op; // The ALU operation specified
+wire decode_or_pc; // Current program counter
+wire decode_flush; //Decode Flush
+wire decode_stall;
+);
 
-//Wires for alu
-wire [31:0] alu_or_rs1; //Source register 1 value
-wire [31:0] alu_or_rs2; //Source register 2 value
-wire [11:0] alu_or_imm; //Immediate value
-wire [2:0] alu_or_funct3; // function type
-wire [4:0] alu_r_rs1_addr; //address for register source 1
-wire [6:0] alu_or_opcode; //opcode type 
-wire [`XLEN-1:0] alu_ow_result;// ALU result
+//Wires for fetch
+wire [31:0] fetch_inst; // Output instruction for decode
+wire fetch__mem_req; // Request action 
+wire wfetch__stall_program_mem; //Stall off-board mem
+wire [31:0] fetch_pc; 
+
+// Wires for execute
+
+wire execute_or_opcode; // Opcode
+wire execute_or_funct3; // Funct3
+wire execute_or_rs2_data; // Data to be written
+wire execute_or_rd_addr; // Destination register address
+wire execute_or_rd_wr_en; // RD write enable
+wire execute_or_alu_result;
+wire execute_or_pc; // Current program counter
+wire execute_or_pc_next; // Next program counter for jumps
+wire execute_or_pc_jump; // Jump signal
+wire execute_or_stall; // Output stall signal
+wire execute_or_flush; // Output flush signal
+
 
 //Wires for mem access
 wire memory_or_pc; // Current program counter
@@ -63,21 +79,11 @@ wire writeback_or_flush;
 fetch s1( // logic for fetching instruction [FETCH STAGE , STAGE 1]
 .i_clk(i_clk),
 .i_rst_n(i_rst_n),
-.o_iaddr(o_iaddr), //Instruction address
-.o_pc(fetch_pc), //PC value of o_inst
-.i_inst(i_inst), // retrieved instruction from Memory
-.o_inst(fetch_inst), // instruction
-.o_stb_inst(o_stb_inst), // request for instruction
-.i_ack_inst(i_ack_inst), //ack (high if new instruction is ready)
-// PC Control
-.i_writeback_change_pc(writeback_change_pc), //high when PC needs to change when going to trap or returning from trap
-.i_writeback_next_pc(writeback_next_pc), //next PC due to trap
-.i_alu_change_pc(alu_change_pc), //high when PC needs to change for taken branches and jumps
-.i_alu_next_pc(alu_next_pc), //next PC due to branch or jump
-/// Pipeline Control ///
-.o_ce(decoder_ce), // output clk enable for pipeline stalling of next stage
-.i_stall((stall_decoder || stall_alu || stall_memoryaccess || stall_writeback)), //informs this stage to stall
-.i_flush(decoder_flush) //flush this stage
+.i_branch_sig(execute_or_pc_jump),
+.i_flush_data(executre_or_pc_next),
+.i_stall(execute_or_stall), 
+.i_inst_mem(pm_instruction)
+
 ); 
 
 decode s2( // Logic for decode
@@ -85,66 +91,66 @@ decode s2( // Logic for decode
 .i_rst_n(i_rst_n),
 .i_inst(fetch_inst), //32 bit instruction
 .i_pc(fetch_pc), //PC value from fetch stage
-.or_rs1_addr(decoder_rs2_addr_q), // Source register 1 address for forwarding
-.or_rs2_addr(decoder_rs2_addr_q), // Source register 2 address for forwarding
-.or_rd_addr(decoder_rd_addr),
-.or_imm(decoder_imm), // Does this need to be a wire in decode??
-.or_funct7(decoder_funct3), //Function3 
-.or_opcode(decoder_opcode), //Opcode
-.or_alu_op(decoder_alu) //Alu 
+// Do we need the rd_addr or rd_data??
 //Insert stalls here
 );
 
-alu s3(//Alu Logic
-// Have the ALU operation changed HERE//
-.i_alu_op(decoder_alu), //Alu Operation
-.i_data_1(rs1), //Alu Data
-.i_data_2(rs2), //This needs to be changed, does it need more its own funct3, rs1, rd?
-
-
-
-.i_rs1_addr(decoder_rs1_addr_q), //address for register source 1
-.i_imm_de(decoder_imm), //Immediate value from decode
-.i_funct3_de(decoder_funct3), //function type from decode
-.i_opcode_de(decoder_opcode), //opcode type from decode
-
-.or_rs1(alu_rs1), //Source register 1 value
-.or_rs2(alu_rs2), //Source register 2 value
-.or_imm(alu_imm), //Immediate value
-.or_funct3(alu_funct3), // function type
-.or_rs1_addr(alu_rs1_addr), //address for register source 1
-.or_opcode(alu_opcode), //opcode type 
-.ow_result(alu_y) //Alu result
+execute s3(//Alu Logic
+.i_clk(i_clk), // CPU clock
+.i_rst_n(i_rst_n), // Active low reset
+.i_opcode(decode_or_opcode), // Opcode
+.i_rd_addr(decode_or_rd_addr) // Destination register address
+.i_rd_wr_en(writeback_ir_wd_write),
+.i_rd_mem(memory_or_rd_data), // RD value for instruction currently in the memory stage
+.i_rd_addr_mem(memory_or_addr), // RD address for instruction currently in the memory stage
+.i_rd_mem_wr_en(memory_or_rd_write),  // Register file write enable for the instruction currently in the memory stage 
+.i_rd_wb(writeback_ir_rd_data), // RD value for instruction currently in the write back stage
+.i_rd_addr_wb(writeback_ir_rd_addr), // RD address for instruction currently in the write back stage
+.i_rd_wb_wr_en(writeback_ir_rd_write),  // Register file write enable for the instruction currently in the write back stage
+//Do these need to be in here? and is the above a copy?
+.i_rs1_addr, // Source register 1 address for forwarding
+.i_rs2_addr, // Source register 2 address for forwarding
+.i_rs1_data, // Source register 1 value
+.i_rs2_data, // Source register 2 value
+.i_imm(decode_or_imm), // Immediate value
+.i_funct7(decode_or_funct7), // Funct7
+.i_funct3(decode_or_funct3, // Funct3
+.i_alu_op(decode_or_alu_op), // The ALU operation specified
+.i_pc(decode_or_pc), // Current program counter
+.i_flush(memory_or_flush), // Flush signal from external stages
+.i_stall(memory_or_stall), // Stall signals from external stages
 
 );
 
 memory s4 (//Logic For memory
 /* Memory stage inputs */
-    .i_opcode(alu_or_opcode), // Opcode
-    .i_funct3(alu_funct3),
-    .i_rs2(alu_or_rs2), // Data to be written
-    .i_rd_addr(decoder_rd_addr), // Destination register address
-    //Fix write enable.i_rd_write(decoder_), // Write enable for rd
-    .i_alu_result(alu_ow_result),
-    .i_pc(decoder_pc), // Current program counter
-    //Fix mem awk.i_mem_ack(), // Acknoledgement from memory (data is ready for read/written to)
-    //Fetch ??.i_mem_data(), // Data from memory address requested
-    //Add Flush .i_flush, // Flush signal from external stages
-    //Add Stall .i_stall(), // Stall signals from external stages
-    
-    /* Memory stage outputs */
-    .or_pc(memory_or_pc), // Current program counter
-    .or_rd_addr(memory_or_rd_addr), // Register to be writen to
-    .or_rd_write(memory_or_rd_write), // Write enable for rd
-    .or_rd_data(memory_or_rd_data), // Data to be written to rd
-    .or_opcode(memory_or_opcode), // Opcode
-    .or_mem_req(memory_or_mem_req), // Memory request signal
-    .or_mem_addr(memory_or_mem_addr), // Memory address requested for read/write
-    .or_mem_data(memory_or_mem_data), // Data to be written to memory
-    .or_funct3(memory_or_funct3), // Indicates what kind of memory operation is being performed
-    .or_read_write(memory_or_read_write), // Indicates read (0) or write (1) operation
-    // Do flush output reg or_flush(), // Flush signal to external stages
-    // Do stall output reg or_stall // Stall signal to external stages
+,i(i_clk),
+.i(i_rst_n),
+.i_opcode(alu_or_opcode), // Opcode
+.i_funct3(alu_funct3),
+.i_rs2(alu_or_rs2), // Data to be written
+.i_rd_addr(decoder_rd_addr), // Destination register address
+//Fix write enable.i_rd_write(decoder_), // Write enable for rd
+.i_alu_result(alu_ow_result),
+.i_pc(decoder_pc), // Current program counter
+//Fix mem awk.i_mem_ack(), // Acknoledgement from memory (data is ready for read/written to)
+//Fetch ??.i_mem_data(), // Data from memory address requested
+//Add Flush .i_flush, // Flush signal from external stages
+//Add Stall .i_stall(), // Stall signals from external stages
+
+/* Memory stage outputs */
+.or_pc(memory_or_pc), // Current program counter
+.or_rd_addr(memory_or_rd_addr), // Register to be writen to
+.or_rd_write(memory_or_rd_write), // Write enable for rd
+.or_rd_data(memory_or_rd_data), // Data to be written to rd
+.or_opcode(memory_or_opcode), // Opcode
+.or_mem_req(memory_or_mem_req), // Memory request signal
+.or_mem_addr(memory_or_mem_addr), // Memory address requested for read/write
+.or_mem_data(memory_or_mem_data), // Data to be written to memory
+.or_funct3(memory_or_funct3), // Indicates what kind of memory operation is being performed
+.or_read_write(memory_or_read_write), // Indicates read (0) or write (1) operation
+// Do flush output reg or_flush(), // Flush signal to external stages
+// Do stall output reg or_stall // Stall signal to external stages
 );
 
 
